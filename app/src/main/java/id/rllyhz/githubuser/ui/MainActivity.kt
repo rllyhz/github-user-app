b1dbefa00dc761c55d8fd335c2b221980b442d00
rllyhz <rullyihza00@gmail.com>
2021-03-18 21:30:12 +0700

    Initial commit

---
+++ app/src/main/java/id/rllyhz/githubuser/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package id.rllyhz.githubuser.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import id.rllyhz.githubuser.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
