77db32818df1266216cbe37674f6ab74d92ec518
rllyhz <rullyihza00@gmail.com>
2021-03-19 02:00:10 +0700

    Creating DataUtils.kt file for providing the static data

--- app/src/main/java/id/rllyhz/githubuser/utils/DataUtils.kt
+++ app/src/main/java/id/rllyhz/githubuser/utils/DataUtils.kt
@@ -1,5 +1,36 @@
 package id.rllyhz.githubuser.utils
 
-object DataUtils {
-    val getUsers get() = {}
+import android.content.Context
+import id.rllyhz.githubuser.R
+import id.rllyhz.githubuser.data.User
+
+class DataUtils(private val context: Context) {
+    private val users = ArrayList<User>()
+
+    fun getUsers(): List<User> {
+        val usernames = context.resources.getStringArray(R.array.username)
+        val names = context.resources.getStringArray(R.array.name)
+        val locations = context.resources.getStringArray(R.array.location)
+        val repositories = context.resources.getStringArray(R.array.repository)
+        val companyNames = context.resources.getStringArray(R.array.company)
+        val followers = context.resources.getStringArray(R.array.followers)
+        val followings = context.resources.getStringArray(R.array.following)
+
+        for (position in usernames.indices) {
+            val user = User(
+                usernames[position].toInt(),
+                names[position],
+                usernames[position],
+                companyNames[position],
+                followers[position],
+                followings[position],
+                repositories[position],
+                locations[position]
+            )
+
+            users.add(user)
+        }
+
+        return users
+    }
 }
