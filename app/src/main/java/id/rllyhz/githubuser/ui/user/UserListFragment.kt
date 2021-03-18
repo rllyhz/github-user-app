98c43a82b42bc93635007f78ec5734d2a3a4354b
rllyhz <rullyihza00@gmail.com>
2021-03-18 22:25:45 +0700

    Adding animation files, such as slide_in_left.xml, slide_in_right.xml, slide_out_left.xml, slide_out_right.xml

--- app/src/main/java/id/rllyhz/githubuser/ui/user/UserListFragment.kt
+++ app/src/main/java/id/rllyhz/githubuser/ui/user/UserListFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import id.rllyhz.githubuser.databinding.UserListFragmentBinding
+import kotlinx.android.synthetic.main.user_list_fragment.*
 
 class UserListFragment: Fragment() {
     private var _binding: UserListFragmentBinding? = null
@@ -18,6 +20,15 @@ class UserListFragment: Fragment() {
     ): View? {
         _binding = UserListFragmentBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btn_click_me.setOnClickListener {
+            val action = UserListFragmentDirections.actionUserListFragmentToUserDetailFragment()
+            findNavController().navigate(action)
+        }
     }
 
     override fun onDestroy() {
