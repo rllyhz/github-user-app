b15beb7e53755ce9d2124930c2a1b920a08b4cd8
rllyhz <rullyihza00@gmail.com>
2021-03-18 21:32:56 +0700

    Initial Commit

--- app/src/main/java/id/rllyhz/githubuser/ui/MainActivity.kt
+++ app/src/main/java/id/rllyhz/githubuser/ui/MainActivity.kt
@@ -2,12 +2,14 @@ package id.rllyhz.githubuser.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import id.rllyhz.githubuser.R
+import id.rllyhz.githubuser.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
