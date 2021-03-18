a0b3eb378e81b0c334b533e8341c9d521e2798d6
rllyhz <rullyihza00@gmail.com>
2021-03-19 02:28:04 +0700

    Updating UserListAdapter.kt file

--- app/src/main/java/id/rllyhz/githubuser/adapter/UserListAdapter.kt
+++ app/src/main/java/id/rllyhz/githubuser/adapter/UserListAdapter.kt
@@ -1,3 +1,45 @@
 package id.rllyhz.githubuser.adapter
 
-class UserListAdapter
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import id.rllyhz.githubuser.data.User
+import id.rllyhz.githubuser.databinding.ItemUserBinding
+
+class UserListAdapter : RecyclerView.Adapter<UserListAdapter.UserListViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserListViewHolder {
+        val binding = ItemUserBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return UserListViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: UserListViewHolder, position: Int) {
+        holder.bind(differ.currentList[position])
+    }
+
+    override fun getItemCount() = differ.currentList.size
+
+    // ViewHolder
+    inner class UserListViewHolder(private val binding: ItemUserBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(user: User) {
+            //
+        }
+    }
+
+    // Callback for DifferUtil
+    private val differCallback = object : DiffUtil.ItemCallback<User>() {
+        override fun areItemsTheSame(oldItem: User, newItem: User): Boolean {
+            return oldItem.username == newItem.username
+        }
+
+        override fun areContentsTheSame(oldItem: User, newItem: User): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    private val differ = AsyncListDiffer(this, differCallback)
+}
