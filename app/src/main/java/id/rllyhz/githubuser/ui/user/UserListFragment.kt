75c2baa390d3931dd29f06dd644dfb81a408926d
rllyhz <rullyihza00@gmail.com>
2021-03-18 22:06:20 +0700

    setting up the NavHostContainer using FragmentContainerView (navigation set up)

---
+++ app/src/main/java/id/rllyhz/githubuser/ui/user/UserListFragment.kt
@@ -0,0 +1,27 @@
+package id.rllyhz.githubuser.ui.user
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import id.rllyhz.githubuser.databinding.UserListFragmentBinding
+
+class UserListFragment: Fragment() {
+    private var _binding: UserListFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = UserListFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
