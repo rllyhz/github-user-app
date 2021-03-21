e889fec727898c7339f1dac2febb7c31bad5321c
rllyhz <rullyihza00@gmail.com>
2021-03-21 23:43:11 +0700

    Setting up the splash screen

--- app/src/main/java/id/rllyhz/githubuser/ui/MainActivity.kt
+++ app/src/main/java/id/rllyhz/githubuser/ui/MainActivity.kt
@@ -2,6 +2,8 @@ package id.rllyhz.githubuser.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import id.rllyhz.githubuser.R
 import id.rllyhz.githubuser.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -9,7 +11,12 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setTheme(R.style.Theme_GithubUser) // remove splash theme, for easily showing splashscreen if there is no process in background
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO) // this app dosn't support dark mode
+
         setContentView(binding.root)
     }
 }
